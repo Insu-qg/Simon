ef5c1b13a5f8d1c29341c06e9bec497f6d7025ce
Insu-qg <quent.grange@gmail.com>
2024-02-14 15:52:48 +0100

    ajout pwa

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,33 +8,32 @@ const colors = ['red', 'blue', 'green', 'yellow']
 function App() {
   const [sequence, setSequences] = useState(["red", "blue", "red"]);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [userInput, setUserInput] = useState([]);
   const [isPlayerTurn, setIsplayerTurn] = useState(false);
   
-  // useEffect(() => {
-  //   if (sequence.length > 0 && isPlayerTurn) {
-  //     setTimeout(() => {
-  //       setCurrentIndex();
-  //     }, 1000);
-  //   }
-  // }, [sequence, isPlayerTurn]);
+  useEffect(() => {
+    if (sequence.length > 0 && !isPlayerTurn) {
+      setTimeout(() => {
+        setCurrentIndex(currentIndex + 1);
+      }, 1000);
+    }
+    if( currentIndex >= sequence.length && !isPlayerTurn) {
+      setIsplayerTurn(true)
+      setCurrentIndex(0)
+      console.log("PLAYER TURN")
+    }
+  }, [currentIndex]);
 
-  // const playSequence = () => {
-  //   let i = 0;
-  //   const interval = setInterval(() => {
-  //     highlightColor(sequence[i]);
-  //     i++;
-  //     if (i >= sequence.length) {
-  //       clearInterval(interval);
-  //       setIsplayerTurn(false);
-  //     }
-  //   }, 1000);
-  // };
-
-  // const highlightColor = (color: string) => {
-  //   console.log(`Highlight: ${color}`);
-  // };
-
+  const handleColorClick = useCallback((color: string) => {
+    if (isPlayerTurn && !(currentIndex >= sequence.length)) {
+        if(sequence[currentIndex] == color) {
+          console.log("COULEUR CORRECT : " + color);
+          setCurrentIndex(currentIndex + 1)
+        }else{
+          console.log("MAUVAISE COULEUR: " + color);
+          setIsplayerTurn(false)
+        }
+      }
+    }, [currentIndex])
 
   return (
       <div className='button-container'>
@@ -43,6 +42,7 @@ function App() {
               className="color-button"
               key={color}
               style={{ backgroundColor: (!isPlayerTurn && sequence[currentIndex] === color) ? "purple" : color}}
+              onClick={()=>handleColorClick(color)}
             />
           ))}
       </div>
